8c7cbda8abba4e313ad484fd13758ae14bf2002a
Admin <nort2007@gmail.com>
2024-04-30 12:47:08 +0300

    JS Homework #1 was finished

---
+++ HW 1/HW_1.1.js
@@ -0,0 +1,30 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
---
+++ HW 1/HW_1.2.js
@@ -0,0 +1,10 @@
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = "Mykhailo";
+let middleName = "Victorovych";
+let lastName = "Folomeiev";
+let space = ' ';
+
+let result = firstName + space + middleName + space + lastName;
+
+console.log(result);
